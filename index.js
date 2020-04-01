4b7caac031405b492ce4a0d07cc500548f1f6638
matt-winzer <matt.winzer@gmail.com>
2020-03-31 18:48:47 -0600

    added morgan and cors middleware

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware configuration
+app.use(morgan('dev'));
+app.use(cors());
 
 // Router imports
 const booksRoutes = require('./routes/books');
 
+// Test route
 app.get('/', (req, res, next) => {
   res.json({
     message: 'Hey your server works!'
   });
 });
 
+// Routes
 app.use('/api/books', booksRoutes);
 
 
